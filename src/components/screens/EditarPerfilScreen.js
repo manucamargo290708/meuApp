90ee72f35d5a9aa7c3a2d3c02f2a26ed9717b098
manucamargo29 <manu.camargo290708@gmail.com>
2026-06-09 15:53:58 -0300

    alteração da segurança da senha

--- src/components/screens/EditarPerfilScreen.js
+++ src/components/screens/EditarPerfilScreen.js
@@ -42,7 +42,6 @@ export default function EditarPerfilScreen() {
             style={styles.input}
             placeholder="********"
             placeholderTextColor="#412f51"
-            secureTextEntry
           />
 
           <Text style={styles.label}>Telefone</Text>
